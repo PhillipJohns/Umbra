02e8aeb100c263f0cfa66611b9a4483fe71418a4
Tony Lu <tony.lu@utexas.edu>
2020-09-28 16:43:30 -0500

    added textbox

--- Scene1.js
+++ Scene1.js
@@ -11,6 +11,7 @@ var text1;
 var npc_text;
 var player1 = {name: "name1", inventory: []};
 var npc1 = {name: "npc1", dialogue: {1: 'Can you find my box?', 2: 'Thank you!'}};
+var graphics;
 
 // items
 var box_added = false;
@@ -19,8 +20,8 @@ class Scene1 extends Phaser.Scene {
     constructor()
     {
         super('Scene1');
-        
-        
+
+
     }
 preload(){
     this.load.spritesheet('character', 'Free/Main Characters/Ninja Frog/Idle (32x32).png', {frameWidth: 32, frameHeight: 32});
@@ -32,16 +33,16 @@ preload(){
     this.load.image('floor', 'Interior/Floor.png');
     this.load.image('sideWall', 'Interior/SideWall.png');
     // add item image
-    // 28 X 24 
+    // 28 X 24
     this.load.image('box', 'Free/Items/Boxes/Box1/Idle.png')
-    
+
     // add the npc
     this.load.image('npc1', 'Scifi Character/jump.png');
-    
+
 }
 
 create(){
-    
+
     let platforms = this.physics.add.staticGroup();
     //Make background
     y = 30
@@ -52,7 +53,7 @@ create(){
         y += 30
     }
     for (y = 15; y < 700; y += 15){
-        platforms.create(10, y, 'sideWall');   
+        platforms.create(10, y, 'sideWall');
     }
     for (y = 15; y < 700; y += 15){
         platforms.create(790, y, 'sideWall');
@@ -64,26 +65,29 @@ create(){
         platforms.create(x, 0, 'wall').setScale(1).refreshBody();
     }
     //Game Text
-    text1 = this.add.text(30, 30, 'You picked up the box!', { fontSize: '32px', fill: '#900' }).setVisible(false);
+    graphics = this.add.graphics();
+    graphics.fillStyle(0x000000, 1);
+    graphics.fillRect(25, 525, 750, 500).setVisible(false);
+    text1 = this.add.text(50, 545, 'You picked up the box!', { fontSize: '32px', fill: '#999' }).setVisible(false);
     info = this.add.text(350, 60, 'Spacebar to interact', { fontSize: '32px', fill: '#900' });
     info2 = this.add.text(350, 90, 'Shift to end message', { fontSize: '32px', fill: '#900' });
 
     //Make Character
-    
-    npc_text = this.add.text(30, 30, 'Hello there! Welcome to the tutorial!', { fontSize: '32px', fill: '#900' }).setVisible(false);
+
+    npc_text = this.add.text(50, 545, 'Hello there! Welcome to the tutorial!', { fontSize: '32px', fill: '#999' }).setVisible(false);
     player = this.physics.add.sprite(100, 450, 'character');
-    
+
     // border sprite
     let border_sprite = this.physics.add.sprite(300, 300);
     border_sprite.width = 32;
     border_sprite.height = 28;//(29, 26);
-    
+
     // border npc
     let border = this.physics.add.sprite(200, 450);
     border.width = 36;
     border.height = 36;//(29, 26);
 
-    
+
     //animations
     this.anims.create({
         key: 'Idle',
@@ -112,28 +116,28 @@ create(){
     cursors = this.input.keyboard.createCursorKeys();
     var isSpaceDown = cursors.space.isDown;
     this.physics.add.collider(player, platforms);
-    
-    
+
+
     // make npc 1
     npc = this.physics.add.staticGroup();
     npc.create(200, 450, 'npc1');
     npc.width = 32;
     npc.height = 32;
-    
+
     //Make item
     // Player collision causes item to become white
     // Spacebar starts item interaction
     items = this.physics.add.staticGroup();
-    
+
     items.create(300, 300, 'box');
-    
+
 //    this.physics.add.collider(player, items);
     items.setTint(0xff0000)
     this.physics.add.overlap(player, border_sprite, gameItem, null, this);
     this.physics.add.overlap(player, border, gameNpc, null, this);
-    
+
     // function to overlap item
-    
+
     // BUG = BOX is still there, even if invisible
     function gameItem(){
         inZone = true;
@@ -141,17 +145,18 @@ create(){
         // console.info("overlap");
         if(cursors.space.isDown){
             text1.setVisible(true);
+            graphics.setVisible(true);
             if(!box_added){
               player1.inventory.push(new Item("box", "box", text1));
               console.info(player1.inventory);
               box_added = true;
               items.setVisible(false);
-              
+
             }
             this.physics.pause();
         }
     }
-    
+
     // NPC function
     function gameNpc(){
         // console.log("!!!");
@@ -161,17 +166,19 @@ create(){
             if(box_added){
                 npc_text.setText(npc1.dialogue[2]);
                 npc_text.setVisible(true);
+                graphics.setVisible(true);
                 console.log(npc1.dialogue[1]);
             }
             else{
                 npc_text.setText(npc1.dialogue[1]);
                 npc_text.setVisible(true);
+                graphics.setVisible(true);
             }
-            this.physics.pause(); 
-        }  
+            this.physics.pause();
+        }
     }
-    
-    
+
+
     // player collides w/item
     this.physics.add.collider(player, items);
     this.physics.add.collider(player, npc);
@@ -182,19 +189,20 @@ create(){
 update(){
     player.setVelocityX(0);
     player.setVelocityY(0);
-    
+
     // resumes game after text is read
     if(cursors.shift.isDown){
             text1.setVisible(false);
             npc_text.setVisible(false);
+            graphics.setVisible(false);
             this.physics.resume();
     }
-    
+
     inZone = false;
-    
+
 //    items.setVelocityX(0);
 //    items.setVelocityY(0);
-    
+
     if (cursors.right.isDown){
         player.setVelocityX(160);
         player.anims.play('right', true);
@@ -220,8 +228,5 @@ update(){
 //    console.info("hello");
 //    //items.tint = "rgba(255, 255, 255)"; //(0xffffff);
 //    items.setTint(0xffffff);
-//    
+//
 //}
-
-
-
