6862eaa420af328f8c76473fc71c521e0239f8d4
Phillip Johns <phillipjohns81@gmail.com>
2020-10-18 20:38:20 -0500

    final level functionality

--- Scene3.js
+++ Scene3.js
@@ -21,6 +21,22 @@ var sprite_x;
 var sprite_y;
 var spriteCoord;
 
+//Power supplies\
+var batteries;
+var battery1;
+var battery2;
+var powerPad1;
+var powerPad2;
+var brokenBattery1;
+var brokenBattery2;
+var repairKit1;
+var repairKit2;
+var repairKit1Found = false;
+var repairKit2Found = false;
+var battery1Touch = false;
+var battery2Touch = false;
+var powerPads
+
 //doors
 var door1;
 var door2;
@@ -61,6 +77,7 @@ preload(){
 }
 
 create(){
+    let platforms = this.physics.add.staticGroup();
     // background
     bg = this.add.image(0, 0, 'bbackground').setScale(6);
     const maze = this.make.tilemap({ key: "maze" });
@@ -94,6 +111,85 @@ create(){
     // added 16 px to X
     this.add.image(1712, 2224, 'button');
     let borderButton3b = this.physics.add.sprite(1712, 2224).setSize(40, 40);
+
+    //Button 4
+    this.add.image(2704, 1584, 'button');
+    let borderButton4 = this.physics.add.sprite(2704, 1584).setSize(40, 40);
+
+    //Button 4b
+    this.add.image(2736, 2288, 'button');
+    let borderButton4b = this.physics.add.sprite(2736, 2288).setSize(40, 40);
+
+    //power supply generation
+    batteries = this.physics.add.group();
+    battery1 = batteries.create(3904, 1920, 'battery').setScale(.4);
+    battery2 = batteries.create(1024, 2944, 'battery').setScale(.4);
+    powerPads = this.physics.add.staticGroup();
+    powerPad1 = powerPads.create(2688, 2528, 'powerSource').setScale(.4).setSize(74,112).setOffset(60,80);
+    powerPad2 = powerPads.create(2080, 2944, 'powerSource').setScale(.4).setSize(74,112).setOffset(60,80);
+    brokenBattery1 = platforms.create(128, 2496, 'fixbattery', 1).setScale(.4).setSize(65,95).setOffset(50,75);
+    let brokenBattery1Border = this.physics.add.sprite(128, 2496).setSize(70, 100);
+    brokenBattery2 = platforms.create(2560, 1280, 'fixbattery', 1).setScale(.4).setSize(65,95).setOffset(50,75);
+    let brokenBattery2Border = this.physics.add.sprite(2560, 1280).setSize(70, 100);
+    repairKit1 = platforms.create(3104, 1664, 'box');
+    let repairKit1Border = this.physics.add.sprite(3104, 1664).setSize(40, 40);
+    repairKit2 = platforms.create(864, 128, 'box');
+    let repairKit2Border = this.physics.add.sprite(864, 128).setSize(40, 40);
+
+    //Add battery to power pad
+    function addBattery1() {
+        if(!battery1Touch){
+            powerPad1.anims.play('engineOn', true);
+            battery1Touch = true;
+            battery1.setVisible(false);
+            batteries.remove(battery1);
+            }
+    }
+    function addBattery2() {
+        if(!battery2Touch){
+            powerPad2.anims.play('engineOn', true);
+            battery2Touch = true;
+            battery2.setVisible(false);
+            batteries.remove(battery2);
+            }
+    }
+    //add repair Kit
+    function addRepairKit1() {
+        repairKit1Found = true;
+        platforms.remove(repairKit1);
+        repairKit1.setVisible(false);
+    }
+    function addRepairKit2() {
+        repairKit2Found = true;
+        platforms.remove(repairKit2);
+        repairKit2.setVisible(false);
+    }
+    //Fix Batteries
+    function fixBattery1(){
+        if (cursors.space.isDown){
+            console.log('Fix Battery')
+            if (repairKit1Found){
+                brokenBattery1.setFrame(0);
+                repairKit1Found = false
+            }
+            else if(repairKit2Found){
+                brokenBattery1.setFrame(0);
+                repairKit2Found = false
+            }
+    }
+}
+    function fixBattery2(){
+        if (cursors.space.isDown){
+            if (repairKit1Found){
+                brokenBattery2.setFrame(0);
+                repairKit1Found = false
+            }
+            else if(repairKit2Found){
+                brokenBattery2.setFrame(0);
+                repairKit2Found = false
+            }
+        }
+    }
     
     // create door sprites
 //    let door1 = this.physics.add.sprite(0, 0, 'engine_door').setScale(.5);
@@ -137,6 +233,12 @@ create(){
         frameRate: 10,
         repeat: 0
     });
+    this.anims.create({
+        key: 'engineOn',
+        frames: this.anims.generateFrameNumbers('powerSource', { start: 2, end: 2 }),
+        frameRate: 4,
+        repeat: 0
+    });
     // start pad animation
     // this.anims.create({
     //     key: 'fixed',
@@ -152,7 +254,7 @@ create(){
 
     //Test Door
     // Test Value was 2464(56), 2464(15)
-    let platforms = this.physics.add.staticGroup();
+    
     door2 = platforms.create(2520, 2459, 'engine_door').setScale(.35).setSize(170,70).setOffset(170,80);
     //Test2
     door4 = platforms.create(1432, 2554, 'engine_door').setScale(.35).setSize(170,70).setOffset(170,80);
@@ -223,6 +325,8 @@ create(){
     //Player Collision
     this.physics.add.collider(player, worldLayer);
     this.physics.add.collider(player, platforms);
+    this.physics.add.collider(player, batteries);
+    this.physics.add.collider(worldLayer, batteries);
     
 
     //Player Overlap
@@ -237,6 +341,27 @@ create(){
     
     // border button 3b
     this.physics.add.overlap(player, borderButton3b, function(){open_door.call(this, [door4, door5, door6])}, null, this);
+
+    // border button 4
+    this.physics.add.overlap(player, borderButton4, function(){open_door.call(this, [door7])}, null, this);
+
+    // border button 4b
+    this.physics.add.overlap(player, borderButton4b, function(){open_door.call(this, [door2])}, null, this);
+
+    //Battery1 
+    this.physics.add.overlap(battery1, powerPad1, addBattery1, null, this);
+    //Battery2
+    this.physics.add.overlap(battery2, powerPad2, addBattery2, null, this);
+
+    //RepairKit1
+    this.physics.add.overlap(player, repairKit1Border, addRepairKit1, null, this);
+    //RepairKit2
+    this.physics.add.overlap(player, repairKit2Border, addRepairKit2, null, this);
+
+    //brokenBattery1
+    this.physics.add.overlap(player, brokenBattery1Border, fixBattery1, null, this);
+    //brokenBattery2
+    this.physics.add.overlap(player, brokenBattery2Border, fixBattery2, null, this);
 }
 
 update(){
@@ -248,6 +373,8 @@ update(){
     
     player.setVelocityX(0);
     player.setVelocityY(0);
+    batteries.setVelocityX(0);
+    batteries.setVelocityY(0);
     
 //    if(cursors.shift.isDown){
 //        powerSupplyFixedText.setVisible(false);
